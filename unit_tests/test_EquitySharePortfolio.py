ba57e4ef769721fb371b398ec3b04cb74becfc2b
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2023-09-21 01:16:55 +0300

    Tests for create_dividend_dates

--- unit_tests/test_EquitySharePortfolio.py
+++ unit_tests/test_EquitySharePortfolio.py
@@ -101,7 +101,6 @@ def test_create_dividend_dates_single_bond(equity_share_1):
     assert datetime.date(2023, 9, 1) in dividend_dates[0]
     assert datetime.date(2023, 12, 1) in dividend_dates[0]
 
-
 def test_create_dividend_dates_two_equities(equity_share_1, equity_share_2):
     equity_share_portfolio = EquitySharePortfolio()
     equity_share_portfolio.add(equity_share_1)
@@ -115,16 +114,35 @@ def test_create_dividend_dates_two_equities(equity_share_1, equity_share_2):
 def test_generate_market_value_one_equity(equity_share_1):
     modelling_date = datetime.date(2023, 6, 1)
     system_time = datetime.date(2024, 6, 1)
-    #end_date= datetime.date(2023+50, 6, 1)
     equity_share_portfolio = EquitySharePortfolio()
     equity_share_portfolio.add(equity_share_1)
-    #dividend_dates = equity_share_portfolio.create_dividend_dates(modelling_date, end_date)
     market_value = equity_share_1.generate_market_value(modelling_date, system_time, equity_share_1.market_price, equity_share_1.growth_rate)
     t_manual = (system_time-modelling_date).days/365.5
     market_value_manual = equity_share_1.market_price*(1+ equity_share_1.growth_rate)**t_manual
     assert market_value == market_value_manual 
 
+def test_generate_market_value_two_equities(equity_share_1, equity_share_2):
+    modelling_date = datetime.date(2023, 6, 1)
+    equity_share_portfolio = EquitySharePortfolio()
+    equity_share_portfolio.add(equity_share_1)
+    equity_share_portfolio.add(equity_share_2)
+    dividend_dates = equity_share_portfolio.create_dividend_dates(datetime.date(2023, 6, 12), datetime.date(2023+50, 6, 1))
+    dividend_date_1 = list(dividend_dates[0])[0]
+    dividend_date_2 = list(dividend_dates[1])[0]
+    
+    market_value_1 = equity_share_1.generate_market_value(modelling_date, dividend_date_1, equity_share_1.market_price, equity_share_1.growth_rate)
+    market_value_2 = equity_share_2.generate_market_value(modelling_date, dividend_date_2, equity_share_portfolio.equity_share[1].market_price, equity_share_portfolio.equity_share[1].growth_rate)
+    
+    t_manual_1 = (dividend_date_1-modelling_date).days/365.5
+    t_manual_2 = (dividend_date_2-modelling_date).days/365.5
+    market_value_manual_1 = equity_share_1.market_price*(1+ equity_share_1.growth_rate)**t_manual_1
+    market_value_manual_2 = equity_share_portfolio.equity_share[1].market_price*(1+ equity_share_portfolio.equity_share[1].growth_rate)**t_manual_2
+    assert market_value_1 == market_value_manual_1 
+    assert market_value_2 == market_value_manual_2 
+    assert len(dividend_dates) == 2
 
+
+#def test_generate_terminal_value_one_equity
 
 #def test_create_terminal_cashflow_single_equities(equity_share_1):
 #    equity_share_portfolio = EquitySharePortfolio()
