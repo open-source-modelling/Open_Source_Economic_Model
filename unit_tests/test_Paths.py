cbcdd676f65649d6793720e4a0f73993ebce52a3
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2023-09-24 22:24:40 +0300

    Unit tests for Paths class

---
+++ unit_tests/test_Paths.py
@@ -0,0 +1,14 @@
+from PathsClasses import Paths
+import pytest
+import os
+
+@pytest.fixture
+def paths() -> Paths:
+    paths = Paths()
+    return paths
+
+def test_base_path(paths):
+    assert os.path.exists(paths.base)
+
+def test_intermediate_path(paths):
+    assert os.path.exists(paths.intermediate)
