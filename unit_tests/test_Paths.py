3ff93fb11026c894e6ae7b05485621b1579e027c
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2024-03-03 12:12:29 +0100

    Unit test test_output_path now runs

---
+++ unit_tests/test_Paths.py
@@ -0,0 +1,19 @@
+from PathsClasses import Paths
+import pytest
+
+@pytest.fixture
+def paths() -> Paths:
+    paths = Paths(base_folder="Test folder/")
+    return paths
+
+def test_base_path(paths):
+      assert paths.base == "Test folder/"
+
+def test_intermediate_path(paths):
+    assert paths.intermediate == "Test folder/Intermediate/"
+
+def test_input_path(paths):
+    assert paths.input == "Test folder/Input/"
+
+def test_output_path(paths):
+    assert paths.output == "Test folder/Output/"
